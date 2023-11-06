eb8b4c3e95b2b42b5a9a4828d5990693716b24b4
Chris <sdecuir12@gmail.com>
2023-11-06 02:21:52 -0600

    rework function into seperate functions for each button that effect the onscreen score

--- script.js
+++ script.js
@@ -27,48 +27,59 @@ function playRock(playerSelection, computerSelection)  {
   playerSelection = 'rock';
   computerSelection = getComputerChoice();
   if (computerSelection === 'scissors')  {
-      return ++playerScore && console.log('win');
+      ++playerScore && console.log('win');
     } else if (computerSelection === 'paper')  {
-        return ++compScore && console.log('lose');
+         ++compScore && console.log('lose');
       } else if   (playerSelection == computerSelection)  {
-        return 'tie' && console.log('tie')
-      } else return 'invalid input'
+        console.log('tie')
+      }
+  player.textContent = `${playerScore}`;
+  cpu.textContent = `${compScore}`;
 }
 
 function playPaper(playerSelection, computerSelection)  {
   playerSelection = 'paper';
   computerSelection = getComputerChoice();
   if (computerSelection === 'rock')  {
-      return ++playerScore && console.log('win');
+      ++playerScore && console.log('win');
     } else if (computerSelection === 'scissors')  {
-        return ++compScore && console.log('lose');
+        ++compScore && console.log('lose');
       } else if   (playerSelection == computerSelection)  {
-        return 'tie' && console.log('tie')
-      } else return 'invalid input'
+        console.log('tie')
+      }
+  player.textContent = `${playerScore}`;
+  cpu.textContent = `${compScore}`;
 }
 
 function playScissors(playerSelection, computerSelection)  {
   playerSelection = 'scissors';
   computerSelection = getComputerChoice();
   if (computerSelection === 'paper')  {
-      return ++playerScore && console.log('win');
+      ++playerScore && console.log('win');
     } else if (computerSelection === 'rock')  {
         return ++compScore && console.log('lose');
       } else if   (playerSelection == computerSelection)  {
-        return 'tie' && console.log('tie')
-      } else return 'invalid input'
+        console.log('tie')
+      }
+  player.textContent = `${playerScore}`;
+  cpu.textContent = `${compScore}`;
 }
 
 const rock = document.createElement('button');
 const paper = document.createElement('button');
 const scissors = document.createElement('button');
+const player = document.querySelector('#player');
+const cpu = document.querySelector('#cpu')
 
 rock.textContent = 'rock';
 paper.textContent = 'paper';
 scissors.textContent = 'scissors';
+player.textContent = `${playerScore}`;
+cpu.textContent = `${compScore}`;
 
-document.body.append(rock, paper, scissors);
+document.body.prepend(rock, paper, scissors);
 document.body.style.backgroundColor = 'dimgrey';
+document.body.style.color = 'white'
 
 rock.addEventListener('click', playRock);
 paper.addEventListener('click', playPaper);
